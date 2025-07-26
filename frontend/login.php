d1c4e96b506b617b0919b161195647e15277bcd7
Law ence <fedexexpresssservice0@gmail.com>
2025-07-26 03:44:19 +0100

    Removed redundant session_start() from frontend/login.php

--- frontend/login.php
+++ frontend/login.php
@@ -1,8 +1,5 @@
 <?php
 // This is frontend/login.php
-
-session_start(); // Start the session at the very beginning
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
