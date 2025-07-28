b47fb9b473f577dbb881aa70f0a5318db8106cce
Law ence <fedexexpresssservice0@gmail.com>
2025-07-28 08:52:29 +0100

    Add Composer autoloader inclusions to index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require __DIR__ . '/vendor/autoload.php'; // <-- ADD THIS LINE!
+
+
 // Check if MongoDB extension is loaded
 if (!extension_loaded('mongodb')) {
     die('<h1>FATAL ERROR: MongoDB PHP extension is not loaded!</h1><p>Please check your Dockerfile, build logs, and PHP configuration.</p>');
